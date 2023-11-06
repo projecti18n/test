275bdb1365479d9aeb0ecede76d744e52a5faef6
projecti18n <github.com@androssow.com>
2023-11-06 06:37:01 +0100

    x

--- addnodes.js
+++ addnodes.js
@@ -1,16 +1,16 @@
-(async() => {
+async function addNodes(language, name) {
     const neo4j = require('neo4j-driver');
 
-    const uri = 'neo4j+s://49c32e3b.databases.neo4j.io';
+    const uri = 'neo4j+s://20bf51ed.databases.neo4j.io';
     const user = 'neo4j';
-    const password = 'HcPHPGOGxXAmDUklEEhJrsmbdWkAM5xWyBPvEVGrxM4';
+    const password = 'sDxmHIGtM2xt9wGxVL70-ojf-iudv3PDjqaL47WTVuQ';
     
     // To learn more about the driver: https://neo4j.com/docs/javascript-manual/current/client-applications/#js-driver-driver-object
     const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
 
     try {
-        const person1Name = 'Alice';
-        const person2Name = 'David';
+        const person1Name = language;
+        const person2Name = name;
 
         await createFriendship(driver, person1Name, person2Name);
 
@@ -78,4 +78,4 @@
             await session.close();
         }
     }
-})();
+}();
