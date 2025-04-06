404ae1c5b14ea304d66589b303d8ba986f04277a
projecti18n <github.com@androssow.com>
2025-04-06 05:05:45 +0200

    claude

--- cronjob.sh
+++ cronjob.sh
@@ -4,7 +4,7 @@ mv -v /tmp/test/* /var/www/
 rm -r /tmp/*
 cd /var/www/
 pkill -f node
-npm install express formidable neo4j-driver
+npm install express formidable neo4j-driver --save
 npm init -y
 node server.js
 
